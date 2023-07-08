9d60b4b66a5faa44cf7c72f490a3d008c591d1f8
Bhawna Sharma <bhawna.s@peimedia.com>
2023-07-08 19:11:20 +0530

    Initial commit with P1 and P2 components

---
+++ src/lib/Typography/Paragraph.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const P1 = styled.p`
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const P2 = styled.p`
+  font-size: 14px;
+  line-height: 20px;
+`
