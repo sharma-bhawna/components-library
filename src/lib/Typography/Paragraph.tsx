fb031377790a699fd764c3568a375fcc4bd289fe
Bhawna Sharma <bhawna.s@peimedia.com>
2023-07-26 23:31:48 +0530

    Adds prettier

--- src/lib/Typography/Paragraph.tsx
+++ src/lib/Typography/Paragraph.tsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const P1 = styled.p`
   font-size: 16px;
   line-height: 24px;
-`;
+`
 
 export const P2 = styled.p`
   font-size: 14px;
